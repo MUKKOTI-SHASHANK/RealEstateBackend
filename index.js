7332b3d01bacda0c5fd8c0373725b651fc4f2b49
MUKKOTI-SHASHANK <119873232+MUKKOTI-SHASHANK@users.noreply.github.com>
2023-01-22 22:01:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const userController = require("./routes/userRoute");
 const signupLoginController = require("./routes/signupLoginRoute");
 const cors = require("cors");
-app.use(cors());
+// app.use(cors());
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
@@ -23,7 +23,7 @@ mongoose.connect(
 
 // app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(cors());
 
 
